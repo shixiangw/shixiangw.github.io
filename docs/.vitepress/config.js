712a4ac8cbe61d514cba75a62644b0597eb350ea
shixiangw <shixiangw@foxmail.com>
2024-12-20 10:57:52 +0800

    1

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,6 @@
 export default {
+    title: 'shixiangw',
+    description: 'Ignorance and weakness are not obstacles to survival, arrogance is',
     sitemap: {
       hostname: 'https://shixiangw.github.io'
     },
