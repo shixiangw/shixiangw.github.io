58b330bfc53eec91745511271292f2be18975b34
shixiangw <shixiangw@foxmail.com>
2024-12-20 20:06:18 +0800

    1

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,3 +1,32 @@
+import { generateSidebar } from "vitepress-sidebar";
+
+// https://vitepress-sidebar.cdget.com/zhHans/introduction
+const vitepressSidebarOptions = [
+    {
+      documentRootPath: "docs",
+      scanStartPath: "excerpts",
+      resolvePath: "/excerpts/",
+      includeFolderIndexFile: true,
+      useTitleFromFrontmatter: true,
+      collapsed: true,
+    },
+    {
+      documentRootPath: "docs",
+      scanStartPath: "original-articles",
+      resolvePath: "/original-articles/",
+      useTitleFromFrontmatter: true,
+      collapsed: true,
+    },
+    {
+      documentRootPath: "docs",
+      scanStartPath: "verified-articles",
+      resolvePath: "/verified-articles/",
+      useTitleFromFrontmatter: true,
+      collapsed: true,
+    },
+  ];
+
+
 export default {
     title: 'shixiangw',
     description: 'Ignorance and weakness are not obstacles to survival, arrogance is',
@@ -10,5 +39,29 @@ export default {
             light: 'github-dark',
         }
     },
-    ignoreDeadLinks: true
+    vite: {
+      plugins: [
+      //   AutoSidebar({
+      //     ignoreList: ["README.md",".vitepress","public"], // 忽略文件夹
+      //     path: "/docs",
+      //     ignoreIndexItem: false,
+      //     collapsed: false,
+      //     deletePrefix: "",
+      //     sideBarResolved: (data) => data,
+      //     sideBarItemsResolved: (data) => data,
+      //     beforeCreateSideBarItems: (data) => data.sort(),
+      //     titleFromFile: false,
+      // }),
+      ],
+    },
+    // ignoreDeadLinks: true
+    themeConfig: {
+      logo: '/logo.png',
+      nav: [{ text: "home", link: "/" }],
+      socialLinks: [
+        { icon: 'github', link: 'https://github.com/shixiangw' }
+      ],
+      sidebar: generateSidebar(vitepressSidebarOptions),
+    }
   }
+
